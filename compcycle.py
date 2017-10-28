ea98531821e97c8c9056d53312e6945a2319bee2
mattjdelia <mattjdelia@gmail.com>
2017-10-28 22:38:44 +0900

    Update compcycle.py

--- compcycle.py
+++ compcycle.py
@@ -1,17 +1,16 @@
 '''
 Given a list of integers, write a function that determines whether the list consists of a "single complete cycle"
-A list X consists of a "single complete cycle" if and only if, given
-    X is a set (list) containing some number of integers x indexed by a set I (as all Python lists are), and
+A set (list) X consists of a "single complete cycle" if and only if, given
+    X is a set containing some number of integers x indexed by a set I (as all Python lists are), and
     N is a set of iterators n (consecutive integers; though also implied in Python),
     the following function is a bijection from N to X (the set of iterators N is to return each integer in X once and only once)
     If n = 1: i = 0, f(n) = x.i
     If 1 < n <= len(X): i = f(n - 1), f(n) = x.i
  
- E.g., given a set [1, 5, 2], 
+ E.g., given [1, 5, 2] (a list of length 3), 
      n = 1: i = 0, f(n) = 1
      n = 2: i = f(n - 1), f(n) = 5
-     n = 3: i = f(n - 1), f(n) = 2
- 
+     n = 3: i = f(n - 1), f(n) = 2 
 '''
 
 
